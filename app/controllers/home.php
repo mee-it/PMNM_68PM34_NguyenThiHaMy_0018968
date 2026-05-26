b8c2dc940420d4b30d71441ba5571417a09be283
mee-it <nmy2490@gmail.com>
2026-05-26 17:40:42 +0700

    fix

--- app/controllers/home.php
+++ app/controllers/home.php
@@ -4,14 +4,14 @@ class home
   public function index()
   {
     echo "Đây là trang chủ";
-    
   }
 
   public function about()
   {
     echo "Đây là trang giới thiệu";
   }
-  public function login(){
-   require_once'../app/views/home/login.php'; 
+  public function login()
+  {
+    require_once '../app/views/home/login.php';
   }
 }
