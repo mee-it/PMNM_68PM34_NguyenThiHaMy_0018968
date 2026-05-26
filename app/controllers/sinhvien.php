0810435ea02fc31779fc09f314ce4990307d07a8
mee-it <nmy2490@gmail.com>
2026-05-26 17:37:11 +0700

    feat: Hiển thị danh sách sinh viên

--- app/controllers/sinhvien.php
+++ app/controllers/sinhvien.php
@@ -1,14 +1,19 @@
 <?php
-class sinhvien
+require_once '../app/core/Controller.php';
+class sinhvien extends Controller
 {
   public function index()
   {
-   //trả về view
-   require_once'../app/views/sinhvien/index.php';
+    $sinhvienModel = $this->model('sinhvienModel');
+    $sinhviens = $sinhvienModel->getAllSinhVien();
+    //trả về view
+
+    //require_once '../app/views/sinhvien/index.php';
+    $this->view('sinhvien/index', ['sinhviens' => $sinhviens, 'title' => 'Danh sách sinh viên']);
   }
 
   public function create()
   {
-    require_once'../app/views/sinhvien/create.php';
+    require_once '../app/views/sinhvien/create.php';
   }
 }
--- app/views/sinhvien/index.php
+++ app/views/sinhvien/index.php
@@ -4,11 +4,72 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Danh sách sinh viên</title>
+    <title><?php echo $title; ?> </title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f4f6f9;
+            padding: 30px;
+        }
+
+        h1 {
+            text-align: center;
+            color: #333;
+            margin-bottom: 20px;
+        }
+
+        table {
+            width: 80%;
+            margin: auto;
+            border-collapse: collapse;
+            background-color: white;
+            box-shadow: 0 4px 10px rgba(0, 0, 0, 0.1);
+            border-radius: 10px;
+            overflow: hidden;
+        }
+
+        th {
+            background-color: #4f46e5;
+            color: white;
+            padding: 12px;
+        }
+
+        td {
+            padding: 12px;
+            text-align: center;
+            border-bottom: 1px solid #ddd;
+        }
+
+        tr:nth-child(even) {
+            background-color: #f9f9f9;
+        }
+
+        tr:hover {
+            background-color: #eef2ff;
+            transition: 0.2s;
+        }
+    </style>
 </head>
 
 <body>
-    <h1>Danh sách sinh viên</h1>
+    <h1><?php echo $title; ?> </h1>
+    <table>
+
+        <tr>
+            <th>STT</th>
+            <th>Họ tên</th>
+            <th>Giới tính</th>
+            <th>MSSV</th>
+        </tr>
+        <?php foreach ($sinhviens as $sv): ?>
+            <tr>
+                <td><?php echo $sv['id']; ?></td>
+                <td><?php echo $sv['hoten']; ?></td>
+                <td><?php echo $sv['gioitinh']; ?></td>
+                <td><?php echo $sv['mssv']; ?></td>
+            </tr>
+        <?php endforeach; ?>
+    </table>
 </body>
 
 </html>
